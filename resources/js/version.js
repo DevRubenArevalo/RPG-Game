0ccdff83e1ca1ad181efc74463188d1af8bf43da
Ruben <rarevalo1987@gmail.com>
2025-12-09 00:21:36 -0500

    Bumped Version

--- resources/js/version.js
+++ resources/js/version.js
@@ -1,6 +1,6 @@
 // VERSION CONTROL - Single source of truth
 // Update this version number when deploying new releases
-const VERSION = '0.3.2';
+const VERSION = '0.3.3';
 
 // Export for ES6 modules
 export { VERSION };
